c559dba86eaccd70908fb72b894dde4abb5cff4e
Flicker372 <flicker372@outlook.com>
2024-08-08 15:52:47 +0800

    sm8250-common: libinit: Add support of .cert hides
    Signed-off-by: Flicker372 <flicker372@outlook.com>
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- libinit/libvariant_headers/include/libvariant.h
+++ libinit/libvariant_headers/include/libvariant.h
@@ -19,6 +19,7 @@ struct variant_info {
     std::string marketname;
     std::string mod_device;
     std::string model;
+    std::string cert;
     std::string build_fingerprint;
     std::string hwsku;
 
